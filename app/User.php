c42f804eae95bef075a1053f94deef1d21dd0d36
Mattia Migliorini <mattia@squeezyweb.com>
2016-05-26 16:22:59 +0200

    Install Shinobi and Watchtower Fix #12

--- app/User.php
+++ app/User.php
@@ -1,26 +1,48 @@
 <?php
+/**
+ * User Model file.
+ *
+ * @package KYSS
+ * @copyright 2016 SqueezyWeb
+ * @since 0.1.0
+ */
 
 namespace App;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Caffeinated\Shinobi\Traits\ShinobiTrait;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-class User extends Authenticatable
-{
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
+  /**
+   * Include traits.
+   *
+   * @since 0.1.0
+   */
+  use Authenticatable, CanResetPassword, ShinobiTrait;
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @since 0.1.0
+   * @access protected
+   * @var array
+   */
+  protected $fillable = [
+    'name', 'email', 'password',
+  ];
+
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @since 0.1.0
+   * @access protected
+   * @var array
+   */
+  protected $hidden = [
+    'password', 'remember_token',
+  ];
 }
